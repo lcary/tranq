58363a6cadc8ee65444649c8cac768790c5738c9
Luc Cary <luc.cary@gmail.com>
2018-09-15 13:23:16 -0400

    Prepare additional test cases

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -5,7 +5,7 @@ from src.token import Token, TokenType
 
 
 class TestLexer(unittest.TestCase):
-    def test_demo_text_1(self):
+    def test_oneline_computation(self):
         text = "print: 4 * var + 1"
         lexer = Lexer()
         tokens = lexer.get_tokens(text)
@@ -20,6 +20,40 @@ class TestLexer(unittest.TestCase):
         for i in tokens:
             print(i)
         self.assertEqual(tokens, expect)
+
+    def test_multiline_computation(self):
+        text = """
+        var = 2
+        x = 4 * var"
+        print: x / 3
+        """
+        pass
+
+    def test_collection_of_numbers(self):
+        text = "l = [1, 2, 3]"
+        pass
+
+    def test_nested_collection(self):
+        # test collection of collections
+        text = """
+        l = [
+            [0, 0, 1],
+            [0, 1, 0],
+            [1, 0, 0]
+        ]
+        """
+        pass
+
+    def test_oneline_loop(self):
+        text = "for i in [1, 2, 3]: print: i"
+        pass
+
+    def test_multline_loop(self):
+        text = """
+        for i in [1, 2, 3]:
+        print: i"
+        """
+        pass
 
 
 if __name__ == '__main__':
